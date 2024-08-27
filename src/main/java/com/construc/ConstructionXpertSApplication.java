65adb1acd01f584277100abc78f77e2260001948
ily-123 <elkasmiilham994@gmail.com>
2024-08-27 10:24:15 +0100

    create structure de projet

---
+++ src/main/java/com/construc/ConstructionXpertSApplication.java
@@ -0,0 +1,13 @@
+package com.construc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionXpertSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructionXpertSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/construc/ConstructionXpertSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.construc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionXpertSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
