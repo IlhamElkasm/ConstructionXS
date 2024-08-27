29b32dcb216abfce2f122eca8852462b2a8e236a
ily-123 <elkasmiilham994@gmail.com>
2024-08-27 14:12:00 +0100

    t

--- src/main/java/com/construc/Repository/ProjetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.construc.Repository;
-
-import com.construc.Model.Projet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjetRepository extends JpaRepository<Projet, Long> {
-}
