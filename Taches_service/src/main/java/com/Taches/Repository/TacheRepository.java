0ae735ff0bf2c9c498d339750476740f3d7b392b
ily-123 <elkasmiilham994@gmail.com>
2024-08-28 14:17:42 +0100

    clear

--- Taches_service/src/main/java/com/Taches/Repository/TacheRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Taches.Repository;
-
-import com.Taches.Model.Taches;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TacheRepository extends JpaRepository<Taches, Long> {
-
-}
