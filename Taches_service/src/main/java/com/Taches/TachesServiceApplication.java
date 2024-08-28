9153dd3d5cd4b12be716d06f67f6bd458d1115dd
ily-123 <elkasmiilham994@gmail.com>
2024-08-28 14:26:57 +0100

    create service de tache

---
+++ Taches_service/src/main/java/com/Taches/TachesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Taches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TachesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TachesServiceApplication.class, args);
+	}
+
+}
