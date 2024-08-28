d987f9caa5bd59a712e0f62825b9e846de591be6
ily-123 <elkasmiilham994@gmail.com>
2024-08-28 14:04:11 +0100

    create calsse dto de tache et  table  tache et repository tachehpa

--- Taches_service/src/main/java/com/Taches/Service/ITacheService.java
+++ Taches_service/src/main/java/com/Taches/Service/ITacheService.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 public interface ITacheService {
 
-    TacheDTO createTache(TacheDTO tacheDTO);
-    List<TacheDTO> getAllTaches();
-    TacheDTO updateTache(Long id, TacheDTO tacheDTO);
-    void deleteTache(Long id);
-    TacheDTO convertToDTO(Taches taches);
+//    TacheDTO createTache(TacheDTO tacheDTO);
+//    List<TacheDTO> getAllTaches();
+//    TacheDTO updateTache(Long id, TacheDTO tacheDTO);
+//    void deleteTache(Long id);
+//    TacheDTO convertToDTO(Taches taches);
 }
