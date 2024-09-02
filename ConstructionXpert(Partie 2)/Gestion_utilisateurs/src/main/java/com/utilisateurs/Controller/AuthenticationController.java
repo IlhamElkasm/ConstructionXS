bd7cd1a5fdaab36f2a5daece67ff5918ade6674f
ily-123 <elkasmiilham994@gmail.com>
2024-09-02 15:53:25 +0100

    create class  de controller de Authentication

---
+++ ConstructionXpert(Partie 2)/Gestion_utilisateurs/src/main/java/com/utilisateurs/Controller/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.utilisateurs.Controller;
+
+
+import com.utilisateurs.Dto.AuthenticationRequest;
+import com.utilisateurs.Dto.AuthenticationResponse;
+import com.utilisateurs.Dto.RegisterRequest;
+import com.utilisateurs.Service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@CrossOrigin(origins = "http://localhost:4200/")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ){
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ){
+        return ResponseEntity.ok(authService.authenticate(request));
+
+    }
+}
