5ea54f7395771d86889c1d9a858728fce7226663
ily-123 <elkasmiilham994@gmail.com>
2024-09-02 15:34:41 +0100

    create class  personne et class user et eunam role

---
+++ ConstructionXpert(Partie 2)/Gestion_utilisateurs/src/main/java/com/utilisateurs/Model/User.java
@@ -0,0 +1,13 @@
+package com.utilisateurs.Model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends  Personne{
+}
